12fcf02e0c21c820c71ca55a75cce122737be81c
neerajkumar161 <ennkay161@gmail.com>
2023-10-12 11:46:06 +0530

    chore: initial setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+
+<h3>This is +layout.svelte</h3>
+<nav>
+  <a href="/">home</a>
+	<a href="/about">about</a>
+</nav>
+<!-- slot component for displaying all child components, sibiling +page.svelte as well -->
+<slot />
